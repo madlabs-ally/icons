bacdd17a55f1c6f931f720aa98b1e3a0365995d6
madlabs-ally <madalitsosimbeye173@gmail.com>
2025-04-05 23:02:58 +0200

    Began working on icons page

--- packages/static/index.ts
+++ packages/static/index.ts
@@ -1,2 +1,4 @@
 export { default as menu } from "./src/menu.svg";
 export { default as search } from "./src/search.svg";
+export { default as circleMinus } from "./src/circle-minus.svg";
+export { default as circlePlus } from "./src/circle-plus.svg";
